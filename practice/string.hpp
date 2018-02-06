d745279a7b249f9a9f5ebaed387b19dc7fab34ff
Laveréna Wienclaw <laverena@Renas-TEMPORARY-Macbook-Pro.local>
2018-02-05 20:58:49 -0500

    cleaning things up a bit

--- practice/string.hpp
+++ practice/string.hpp
@@ -1,38 +1,31 @@
 // Implement a value-semantic string class. Your class should support the following syntaxes:
 
-// Move construction and assignment (optional)
-string s3 = std::move(s2);
-s3 = std::move(s1);
-
-// Default construct
-string s0;
-
-// Construct and assign from a literal value
-string s1 = "hello";
-s1 = "foo";
-
-// Copy construct and assign
-string s2 = s1;
-s2 = s1; // s2 == s1
-
-// Compare
-s1 == s1;
-
-// Order
-s1 < s1; // lexicographical comparison
-
-// Object independence
-s2 = "bar"; // Does not change the value of s1
-
 
 // We only really care about strlen, strncpy, and
-//
-// There's a things called Sharing that is very bad for the string
-// If you delete a string of objects, must use delete[] in order to get rid of
-// every piece of memory allocated along with returning the pointer to memory
-class string
+class String
 {
 public:
+    // Default construct: string s;
+    String(const char* s)
+    
+    
+    // Construct and assign from a literal value: string s1 = "hello";
+    //                                            s1 = "foo";
+    
+    // Copy construct and assign
+    // string s2 = s1;
+    // s2 = s1; // s2 == s1
+    
+    // Compare
+    // s1 == s1;
+    
+    // Order: lexicographical comparison
+   // s1 < s1;
+    
+    // Object independence
+    //s2 = "bar"; // Does not change the value of s1
+
+    
     string(const char* s)
     : len(std::strlen(s)), str(new char[len]) // this will not compile
     {
@@ -48,3 +41,10 @@ private:
     char* str;
     
 }
+
+
+
+
+// There's a things called Sharing that is very bad for the string
+// If you delete a string of objects, must use delete[] in order to get rid of
+// every piece of memory allocated along with returning the pointer to memory
